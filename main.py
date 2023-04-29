bb96f017ad7f283ca7a8c4241518835728d14bd2
Y0HM1 <k.joch19@wp.pl>
2023-04-29 02:25:38 +0200

    polaczenie widokow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
-import self
 
+
+from correlations import Correlations
 from measures import Measures
 from chartsView import ChartsView
 
@@ -13,44 +14,35 @@ class DobreWinkoApp:
         master.title("Wino w liczbach")
         master.geometry("800x500")
 
-        # Tworzenie widżetu Notebook
         self.notebook = ttk.Notebook(master)
         self.notebook.pack(fill=tk.BOTH, expand=True)
 
-        # Dodawanie zakładek
         self.page1 = ttk.Frame(self.notebook)
         self.notebook.add(self.page1, text="Wino białe")
 
-        # self.page2 = ttk.Frame(self.notebook)
-        # self.notebook.add(self.page2, text="Wino czerwone")
-
-        # Dodanie suwaka do zakładki "Wino białe"
         self.action_frame = tk.Frame(self.master)
         self.action_frame.pack(side=tk.TOP, padx=10, pady=10)
 
         self.action_label = tk.Label(self.action_frame, text="Wybierz akcję:", font=("Helvetica", 14))
         self.action_label.pack(side=tk.LEFT)
 
-        self.action_var = tk.StringVar()
-        self.action_var.set("Wybierz")
+        self.action_buttons = []
+        self.action_buttons.append(tk.Button(self.action_frame, text="Pokaz dane", command=self.show_table))
+        self.action_buttons.append(tk.Button(self.action_frame, text="Obliczanie miar statystycznych", command=self.show_measures))
+        self.action_buttons.append(tk.Button(self.action_frame, text="Wyznaczanie korelacji", command=self.show_correlations))
+        self.action_buttons.append(tk.Button(self.action_frame, text="Wykresy", command=self.show_charts))
 
-        self.action_options = ["Wybierz", "Pokaz dane", "Obliczanie miar statystycznych ", "Wyzanaczanie korelacji",
-                               "Wykresy"]
-        self.action_menu = ttk.OptionMenu(self.action_frame, self.action_var, *self.action_options,
-                                          command=self.show_table)
-        self.action_menu.pack(side=tk.LEFT, padx=10)
+        for button in self.action_buttons:
+            button.pack(side=tk.LEFT, padx=10)
 
-        # Ramka z tabelką
-        self.table_frame = tk.Frame(self.page1)
+        self.table_frame = tk.Frame(self.master)
         self.table_frame.pack(fill=tk.BOTH, expand=True)
-
 
         self.xscrollbar = tk.Scrollbar(self.table_frame, orient=tk.HORIZONTAL)
         self.xscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
         self.yscrollbar = tk.Scrollbar(self.table_frame, orient=tk.VERTICAL)
         self.yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        # Utworzenie tabeli
         self.table = ttk.Treeview(self.table_frame, yscrollcommand=self.yscrollbar.set,
                                   xscrollcommand=self.xscrollbar.set)
         self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
@@ -58,8 +50,6 @@ class DobreWinkoApp:
         self.xscrollbar.config(command=self.table.xview)
         self.yscrollbar.config(command=self.table.yview)
 
-
-        # Pobieranie danych z pliku CSV wino białe i wstawienie ich do tabeli
         self.data = pd.read_csv("winequality-white .csv", sep=";")
         self.table["columns"] = list(self.data.columns)
         self.table["show"] = "headings"
@@ -68,70 +58,44 @@ class DobreWinkoApp:
         for index, row in self.data.iterrows():
             self.table.insert("", "end", values=list(row))
 
-        # Ukryj tabelkę na początku
         self.table_frame.pack_forget()
 
-    def show_table(self, event):
-        if self.action_var.get() == "Pokaz dane":
-            # Usuń widżety poprzedniej opcji
-            if hasattr(self, "chart"):
-                self.chart.pack_forget()
-            if hasattr(self, "correlations"):
-                self.correlations.pack_forget()
-            # Pokaż tabelkę
-            self.table_frame.pack(fill=tk.BOTH, expand=True)
+    def show_table(self):
+        self.hide_widgets()
+        self.table_frame.pack(fill=tk.BOTH, expand=True)
 
-        elif self.action_var.get() == "Wykresy":
-            # Usuń widżety poprzedniej opcji
-            if hasattr(self, "table_frame"):
-                self.table_frame.pack_forget()
-            if hasattr(self, "correlations"):
-                self.correlations.pack_forget()
-            # Pokaż wykresy
-            self.chart = ChartsView(self.master)
-            self.chart.pack(fill=tk.BOTH, expand=True)
+    def show_charts(self):
+        self.hide_widgets()
+        chart_window = tk.Toplevel(self.master)
+        chart_window.title("Wykresy")
+        chart_window.geometry("800x600")
+        chart = ChartsView(chart_window)
+        chart.pack(fill=tk.BOTH, expand=True)
 
-        elif self.action_var.get() == "Wyzanaczanie korelacji":
-            # Usuń widżety poprzedniej opcji
-            if hasattr(self, "chart"):
-                self.chart.pack_forget()
-            if hasattr(self, "table_frame"):
-                self.table_frame.pack_forget()
-            # Pokaż widok z korelacjami
-            from correlations import Correlations
-            self.correlations = Correlations(self.master)
-            self.correlations.pack(fill=tk.BOTH, expand=True)
+    def show_correlations(self):
+        self.hide_widgets()
+        correlations_window = tk.Toplevel(self.master)
+        correlations_window.title("Korelacje")
+        correlations_window.geometry("800x600")
+        correlations = Correlations(correlations_window)
+        correlations.pack(fill=tk.BOTH, expand=True)
 
-        elif self.action_var.get() == "Obliczanie miar statystycznych":
-            if hasattr(self, "correlations"):
-                self.correlations.pack_forget()
-            if hasattr(self, "table_frame"):
-                self.table_frame.pack_forget()
-            if hasattr(self, "measures"):
-                self.measures.pack_forget()
-        # Pokaż widok z miarami
-        self.measures = Measures(self.master)
-        self.measures.pack(fill=tk.BOTH, expand=True)
+    def show_measures(self):
+        self.hide_widgets()
+        measures_window = tk.Toplevel(self.master)
+        measures_window.title("Miary statystyczne")
+        measures_window.geometry("800x600")
+        measures = Measures(measures_window)
+        measures.pack(fill=tk.BOTH, expand=True)
 
-        # else:
-        #     # Usuń widżety poprzedniej opcji
-        #     if hasattr(self, "chart"):
-        #         self.chart.pack_forget()
-        #     if hasattr(self, "table_frame"):
-        #         self.table_frame.pack_forget()
-        #     if hasattr(self, "correlations"):
-        #         self.correlations.pack_forget()
-        #     if hasattr(self, "measures"):
-        #         self.measures.pack_forget()
-
-    def show_page1(self):
-        self.notebook.select(self.page1)
-
-    def show_page2(self):
-        self.notebook.select(self.page2)
-
+    def hide_widgets(self):
+        if hasattr(self, "correlations"):
+            self.correlations.pack_forget()
+            del self.correlations
+        if hasattr(self, "measures"):
+            self.measures.pack_forget()
+            del self.measures
 
 root = tk.Tk()
 app = DobreWinkoApp(root)
 root.mainloop()
-# self.master.mainloop()
